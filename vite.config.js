832834e6c0d4bda01802cd81f1129b680c3f65cf
vedantj77 <vedantjadhav575@gmail.com>
2025-12-04 22:12:54 +0530

    Merge remot

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Remove or comment out the base line:
-  // base: '/mac_app/'
 })
